afec9222af7f09c3103bf733fe2db4fa62adc494
Sebikostudio <sebikostudio@gmail.com>
2024-06-16 16:08:34 +0800

    add arrows

--- src/Icons.js
+++ src/Icons.js
@@ -173,3 +173,161 @@ export const VectorIcon = forwardRef(({ className, style }, ref) => (
     />
   </svg>
 ));
+
+export const TextIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M4.5 5.5V4C4.5 3.72386 4.72386 3.5 5 3.5L15 3.5C15.2761 3.5 15.5 3.72386 15.5 4V5.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path d="M10 3V17" stroke="currentColor" strokeLinejoin="round" />
+    <path
+      d="M8 16.5L12 16.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const FontItalicIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M12 4.50003L8 15.5" stroke="currentColor" strokeLinejoin="round" />
+    <path
+      d="M5 15.5H11.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M8.5 4.5H15"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const FontBoldIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M11.6282 9.5H6.5M11.6282 9.5C12.4939 9.5 13.5 8.5 13.5 7C13.5 7 13.5 4.5 10.5 4.5H6.5V9.5M11.6282 9.5C13.6795 9.5 14.5 11.2938 14.5 12.6481C14.5 14.0025 13.4946 15.5 11.6282 15.5H6.5V9.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const ArrowLeftIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M4 10H16.5" stroke="currentColor" strokeLinecap="round" />
+    <path
+      d="M9.5 15.5L3.5 10L9.5 4.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const ArrowRightIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M16 10H3.5" stroke="currentColor" strokeLinecap="round" />
+    <path
+      d="M10.5 15.5L16.5 10L10.5 4.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const ArrowUpIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M10 4V16.5" stroke="currentColor" strokeLinecap="round" />
+    <path
+      d="M15.5 9.5L10 3.5L4.5 9.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const ArrowDownIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M10 16V3.5" stroke="currentColor" strokeLinecap="round" />
+    <path
+      d="M15.5 10.5L10 16.5L4.5 10.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
