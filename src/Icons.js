df769495faeced9ddfb7af61217d85be3ba66a2a
Sebikostudio <sebikostudio@gmail.com>
2024-06-17 16:31:33 +0800

    add shapes

--- src/Icons.js
+++ src/Icons.js
@@ -411,13 +411,13 @@ export const AlignBottomIcon = forwardRef(({ className, style }, ref) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      d="M2.5 17.5L17.5 17.5"
+      d="M1.5 18.5L18.5 18.5"
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
-    <rect x="6" y="2" width="3" height="13" rx="1.5" fill="currentColor" />
-    <rect x="11" y="7" width="3" height="8" rx="1.5" fill="currentColor" />
+    <rect x="6" y="3" width="3" height="13" rx="1.5" fill="currentColor" />
+    <rect x="11" y="8" width="3" height="8" rx="1.5" fill="currentColor" />
   </svg>
 ));
 
@@ -433,27 +433,27 @@ export const AlignLeftIcon = forwardRef(({ className, style }, ref) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      d="M2.5 2.5L2.5 17.5"
+      d="M1.5 1.5L1.5 18.5"
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <rect
-      x="18"
+      x="17"
       y="6"
       width="3"
       height="13"
       rx="1.5"
-      transform="rotate(90 18 6)"
+      transform="rotate(90 17 6)"
       fill="currentColor"
     />
     <rect
-      x="13"
+      x="12"
       y="11"
       width="3"
       height="8"
       rx="1.5"
-      transform="rotate(90 13 11)"
+      transform="rotate(90 12 11)"
       fill="currentColor"
     />
   </svg>
@@ -471,7 +471,7 @@ export const AlignRightIcon = forwardRef(({ className, style }, ref) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      d="M17.5 2.5L17.5 17.5"
+      d="M18.5 1.5L18.5 18.5"
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -480,14 +480,14 @@ export const AlignRightIcon = forwardRef(({ className, style }, ref) => (
       width="3"
       height="13"
       rx="1.5"
-      transform="matrix(4.37114e-08 1 1 -4.37114e-08 2 6)"
+      transform="matrix(4.37114e-08 1 1 -4.37114e-08 3 6)"
       fill="currentColor"
     />
     <rect
       width="3"
       height="8"
       rx="1.5"
-      transform="matrix(4.37114e-08 1 1 -4.37114e-08 7 11)"
+      transform="matrix(4.37114e-08 1 1 -4.37114e-08 8 11)"
       fill="currentColor"
     />
   </svg>
@@ -505,7 +505,7 @@ export const AlignTopIcon = forwardRef(({ className, style }, ref) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      d="M2.5 2.5L17.5 2.5"
+      d="M1.5 1.5L18.5 1.5"
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -514,14 +514,14 @@ export const AlignTopIcon = forwardRef(({ className, style }, ref) => (
       width="3"
       height="13"
       rx="1.5"
-      transform="matrix(1 0 0 -1 6 18)"
+      transform="matrix(1 0 0 -1 6 17)"
       fill="currentColor"
     />
     <rect
       width="3"
       height="8"
       rx="1.5"
-      transform="matrix(1 0 0 -1 11 13)"
+      transform="matrix(1 0 0 -1 11 12)"
       fill="currentColor"
     />
   </svg>
@@ -540,7 +540,7 @@ export const AlignCenterVerticallyIcon = forwardRef(
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M2.5 10L17.5 10"
+        d="M1.5 10L18.5 10"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -576,7 +576,7 @@ export const AlignCenterHorizontallyIcon = forwardRef(
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M10 2.5L10 17.5"
+        d="M10 1.5L10 18.5"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -611,13 +611,13 @@ export const StretchVerticallyIcon = forwardRef(({ className, style }, ref) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      d="M2.5 1.5L17.5 1.5"
+      d="M1.5 0.500001L18.5 0.500001"
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M2.5 18.5L17.5 18.5"
+      d="M1.5 19.5L18.5 19.5"
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -632,6 +632,346 @@ export const StretchVerticallyIcon = forwardRef(({ className, style }, ref) => (
   </svg>
 ));
 
+export const SpaceBetweenVerticallyIcon = forwardRef(
+  ({ className, style }, ref) => (
+    <svg
+      ref={ref}
+      className={className}
+      style={style}
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.5 0.500001L18.5 0.500001"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.5 19.5L18.5 19.5"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8 6C8 6.55228 8.44772 7 9 7H11C11.5523 7 12 6.55228 12 6V1H8V6Z"
+        fill="currentColor"
+      />
+      <path
+        d="M8 19H12V14C12 13.4477 11.5523 13 11 13H9C8.44772 13 8 13.4477 8 14V19Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+);
+
+export const ViewGridIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M17.5 18.5L2.5 18.5C1.94772 18.5 1.5 18.0523 1.5 17.5L1.5 2.5C1.5 1.94772 1.94772 1.5 2.5 1.5L17.5 1.5C18.0523 1.5 18.5 1.94772 18.5 2.5L18.5 17.5C18.5 18.0523 18.0523 18.5 17.5 18.5Z"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path d="M10 1.5V18.5" stroke="currentColor" strokeLinejoin="round" />
+    <path d="M18.5 10L1.5 10" stroke="currentColor" strokeLinejoin="round" />
+  </svg>
+));
+
+export const CopyIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M7.5 4.5V2.5H17.5V12.5H15.5M2.5 7.5H12.5V17.5H2.5V7.5Z"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const BoxIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M16.5 18.5L3.5 18.5C2.39543 18.5 1.5 17.6046 1.5 16.5L1.5 3.5C1.5 2.39543 2.39543 1.5 3.5 1.5L16.5 1.5C17.6046 1.5 18.5 2.39543 18.5 3.5L18.5 16.5C18.5 17.6046 17.6046 18.5 16.5 18.5Z"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const SquareIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M17.5 18.5L2.5 18.5C1.94772 18.5 1.5 18.0523 1.5 17.5L1.5 2.5C1.5 1.94772 1.94772 1.5 2.5 1.5L17.5 1.5C18.0523 1.5 18.5 1.94772 18.5 2.5L18.5 17.5C18.5 18.0523 18.0523 18.5 17.5 18.5Z"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const CircleIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M18.5 10C18.5 5.30558 14.6944 1.5 10 1.5C5.30558 1.5 1.5 5.30558 1.5 10C1.5 14.6944 5.30558 18.5 10 18.5C14.6944 18.5 18.5 14.6944 18.5 10Z"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const StarIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M7.5 6.5L10 1.5L12.5 6.5L18.5 7.5L14.5 11.5L15.5 17.5L10 14.5L4.5 17.5L5.5 11.5L1.5 7.5L7.5 6.5Z"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const StarFilledIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M7.5 6.5L10 1.5L12.5 6.5L18.5 7.5L14.5 11.5L15.5 17.5L10 14.5L4.5 17.5L5.5 11.5L1.5 7.5L7.5 6.5Z"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const TriangleIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M18.5 16.5L1.5 16.5L10 2.5L18.5 16.5Z"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const LightningBoltIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M10 11.5H3.5L12.5 1.5L11 7.5H16.5L8.5 18.5L10 11.5Z"
+      stroke="currentColor"
+      stroke-miterlimit="1.414"
+      strokeLinecap="square"
+      strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const SpaceBetweenHorizontallyIcon = forwardRef(
+  ({ className, style }, ref) => (
+    <svg
+      ref={ref}
+      className={className}
+      style={style}
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M19.5 1.5L19.5 18.5"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0.499999 1.5L0.499999 18.5"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14 8C13.4477 8 13 8.44772 13 9L13 11C13 11.5523 13.4477 12 14 12L19 12L19 8L14 8Z"
+        fill="currentColor"
+      />
+      <path
+        d="M1 8L1 12L6 12C6.55228 12 7 11.5523 7 11L7 9C7 8.44772 6.55228 8 6 8L1 8Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+);
+
+export const SpaceEvenlyHorizontallyIcon = forwardRef(
+  ({ className, style }, ref) => (
+    <svg
+      ref={ref}
+      className={className}
+      style={style}
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M19.5 1.5L19.5 18.5"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M0.499999 1.5L0.499999 18.5"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <rect
+        width="4"
+        height="6"
+        rx="1"
+        transform="matrix(-4.37114e-08 1 1 4.37114e-08 11 8)"
+        fill="currentColor"
+      />
+      <rect
+        width="4"
+        height="6"
+        rx="1"
+        transform="matrix(-4.37114e-08 1 1 4.37114e-08 3 8)"
+        fill="currentColor"
+      />
+    </svg>
+  )
+);
+
+export const SpaceEvenlyVerticallyIcon = forwardRef(
+  ({ className, style }, ref) => (
+    <svg
+      ref={ref}
+      className={className}
+      style={style}
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18.5 19.5L1.5 19.5"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.5 0.499999L1.5 0.499998"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <rect
+        width="4"
+        height="6"
+        rx="1"
+        transform="matrix(-1 -8.74228e-08 -8.74228e-08 1 12 11)"
+        fill="currentColor"
+      />
+      <rect
+        width="4"
+        height="6"
+        rx="1"
+        transform="matrix(-1 -8.74228e-08 -8.74228e-08 1 12 3)"
+        fill="currentColor"
+      />
+    </svg>
+  )
+);
+
 export const StretchHorizontallyIcon = forwardRef(
   ({ className, style }, ref) => (
     <svg
@@ -645,13 +985,13 @@ export const StretchHorizontallyIcon = forwardRef(
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M18.5 2.5L18.5 17.5"
+        d="M19.5 1.5L19.5 18.5"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M1.5 2.5L1.5 17.5"
+        d="M0.499999 1.5L0.499999 18.5"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -726,6 +1066,28 @@ export const ArrowDownIcon = forwardRef(({ className, style }, ref) => (
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
+    />
+  </svg>
+));
+
+export const BellTiltIcon = forwardRef(({ className, style }, ref) => (
+  <svg
+    ref={ref}
+    className={className}
+    style={style}
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M2.96239 7.3237L6.29026 7.06771C6.40926 7.05855 6.52117 7.00703 6.60557 6.92263L9.3031 4.22509C11.9468 1.58139 14.5484 2.39933 16.0206 3.87154C17.4928 5.34375 18.3108 7.94535 15.6671 10.5891L12.9694 13.2867C12.885 13.3711 12.8336 13.4829 12.8245 13.6019L12.5685 16.9298C12.5357 17.3557 12.0184 17.547 11.7164 17.245L2.64718 8.17578C2.34512 7.87372 2.53647 7.35646 2.96239 7.3237Z"
+      stroke="currentColor"
+    />
+    <path
+      d="M8.92811 14.4567C7.9518 15.433 6.36889 15.433 5.39258 14.4567C4.41627 13.4804 4.41627 11.8975 5.39258 10.9211"
+      stroke="currentColor"
     />
   </svg>
 ));
