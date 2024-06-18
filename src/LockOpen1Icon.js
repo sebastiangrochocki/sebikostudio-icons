93e79c5ecc344ed751156b77b510a8f137dde5f4
Sebikostudio <sebikostudio@gmail.com>
2024-06-18 15:32:09 +0800

    Update lock icon

--- src/LockOpen1Icon.js
+++ src/LockOpen1Icon.js
@@ -4,7 +4,7 @@
     export const LockOpen1Icon = forwardRef(({ className, style }, ref) => (
       <svg ref={ref} className={className} style={style} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M15.5 18.5L4.5 18.5C3.94772 18.5 3.5 18.0523 3.5 17.5L3.5 10.5C3.5 9.94772 3.94772 9.5 4.5 9.5L15.5 9.5C16.0523 9.5 16.5 9.94772 16.5 10.5L16.5 17.5C16.5 18.0523 16.0523 18.5 15.5 18.5Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
-<path d="M13.5 9.5V5C13.5 3.067 11.933 1.5 10 1.5C8.067 1.5 6.5 3.067 6.5 5V6.5" stroke="currentColor" strokeLinecap="round"/>
+<path d="M6.5 5C6.5 3.067 8.067 1.5 10 1.5C11.933 1.5 13.5 3.067 13.5 5V9.5" stroke="currentColor" strokeLinecap="round"/>
 </svg>
 
     ));
