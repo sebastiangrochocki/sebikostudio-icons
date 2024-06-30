8d6895cc73a74979b144b3752f1e79f93ac5d80f
Sebikostudio <sebikostudio@gmail.com>
2024-07-01 01:26:14 +0800

    Update bell Icon

---
+++ src/Desktop2Icon.js
@@ -0,0 +1,12 @@
+
+    import React, { forwardRef } from 'react';
+
+    export const Desktop2Icon = forwardRef(({ className, style }, ref) => (
+      <svg ref={ref} className={className} style={style} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12.5 14.5L13.5 17.5H6.5L7.5 14.5M2 14.5H18C18.2761 14.5 18.5 14.2761 18.5 14V4C18.5 3.72386 18.2761 3.5 18 3.5H2C1.72386 3.5 1.5 3.72386 1.5 4V14C1.5 14.2761 1.72386 14.5 2 14.5Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
+
+    ));
+
+    export default Desktop2Icon;
+  
---
+++ src/MobileIcon.js
@@ -0,0 +1,12 @@
+
+    import React, { forwardRef } from 'react';
+
+    export const MobileIcon = forwardRef(({ className, style }, ref) => (
+      <svg ref={ref} className={className} style={style} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7.5 15.5H12.5M15.5 2.5V17.5C15.5 18.0523 15.0523 18.5 14.5 18.5H5.5C4.94771 18.5 4.5 18.0523 4.5 17.5V2.5C4.5 1.94772 4.94772 1.5 5.5 1.5H14.5C15.0523 1.5 15.5 1.94772 15.5 2.5Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
+
+    ));
+
+    export default MobileIcon;
+  
